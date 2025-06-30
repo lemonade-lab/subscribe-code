d23dc162ef8e7617d7badfcfed5ad8394c0bbfc8
ningmengchongshui <ningmengchongshui@gmail.com>
2025-07-01 00:18:57 +0800

    fix: work

--- docker-build.sh
+++
@@ -1,5 +0,0 @@
-docker buildx rm gobuilder
-docker buildx create --name gobuilder --config ./buildkitd.toml --use --driver-opt network=host
-docker buildx inspect --bootstrap
-docker build -t alemonjs-code:latest .
-docker exec -it alemonjs-code bash
